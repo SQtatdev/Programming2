d48fbd0205f5293c42e4877b9965703c0ce7548b
Vjatseslav Ryvlin <50703300255@KLASSA302-26>
2024-12-06 14:14:17 +0200

    controllers

---
+++ KooliProjekt/Controllers/TournamentsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KooliProjekt.Data;
+
+namespace KooliProjekt.Controllers
+{
+    public class TournamentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TournamentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tournaments
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.tournaments.ToListAsync());
+        }
+
+        // GET: Tournaments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tournament = await _context.tournaments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tournament == null)
+            {
+                return NotFound();
+            }
+
+            return View(tournament);
+        }
+
+        // GET: Tournaments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tournaments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TournmentName,TournamentStart,TournamentEnd,TournamentPart,TournamentInfo")] Tournament tournament)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tournament);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tournament);
+        }
+
+        // GET: Tournaments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tournament = await _context.tournaments.FindAsync(id);
+            if (tournament == null)
+            {
+                return NotFound();
+            }
+            return View(tournament);
+        }
+
+        // POST: Tournaments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TournmentName,TournamentStart,TournamentEnd,TournamentPart,TournamentInfo")] Tournament tournament)
+        {
+            if (id != tournament.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tournament);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TournamentExists(tournament.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tournament);
+        }
+
+        // GET: Tournaments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tournament = await _context.tournaments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tournament == null)
+            {
+                return NotFound();
+            }
+
+            return View(tournament);
+        }
+
+        // POST: Tournaments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tournament = await _context.tournaments.FindAsync(id);
+            if (tournament != null)
+            {
+                _context.tournaments.Remove(tournament);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TournamentExists(int id)
+        {
+            return _context.tournaments.Any(e => e.Id == id);
+        }
+    }
+}
